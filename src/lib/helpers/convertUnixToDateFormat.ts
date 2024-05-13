21195448d65b999e35fc3c23e1aba32740a7e910
reactnenja <reactnenja@gmail.com>
2024-05-13 10:31:15 +0100

    success nextjs clone

--- src/lib/helpers/convertUnixToDateFormat.ts
+++
@@ -1,6 +0,0 @@
-export function convertUnixToDateFormat(unixDate: number) {
-  return new Date(unixDate * 1000).toLocaleString('en-GB', {
-    timeStyle: 'short',
-    dateStyle: 'medium',
-  });
-}
